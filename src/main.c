6b8f8f1100b217f1716aa1bbd90b42556af64a6e
Andréas Forsbom <andreasforsbom@gmail.com>
2023-03-30 23:07:51 +0200

    Use create_neuron. Added debug print

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
 #include "neuron.h"
 
 int main()
 {
-    Neuron n = createNeuron();
+    Neuron n = create_neuron(10);
+
+    printf("%d\n", n.num_inputs);
+
     return n.num_inputs;
 }
