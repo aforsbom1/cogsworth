bb0ad71e6666f5dcc517d9678d5e3398f427b0a9
Andréas Forsbom <andreasforsbom@gmail.com>
2023-03-30 23:57:05 +0200

    Initialize network

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include "layer.h"
+#include "network.h"
 
 int main()
 {
-    Layer layer = create_layer(3, 4);
+    Network network;
+    initialize_network(&network, 2, 1, 1, 3); // Example: 2 inputs, 1 output, 1 hidden layer with 3 neurons
 
-    printf("%d\n", layer.num_neurons);
+    printf("%d\n", network.num_layers);
 
     return 0;
 }
