3aa30be5eaa9024b0a1ed9e6d23e6005e42db7e3
Andréas Forsbom <andreasforsbom@gmail.com>
2023-04-01 22:31:02 +0200

    Drop out from training at 0.0005 error rate

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 int main()
 {
     // Initialize the network
-    Network network = initialize_network(3, 1, 1, 3);
+    Network network = initialize_network(3, 1, 5, 4);
 
     // Define the test dataset
     double inputs[4][2] = {
@@ -13,39 +13,44 @@ int main()
         {0, 1},
         {1, 0},
         {1, 1}};
-    double expected_outputs[] = {0, 1, 1, 0};
+    double expected_outputs[4] = {0, 0, 0, 1};
+    int isTrained = 0;
+    printf("Training the network...\n");
 
     // Test the network
-    for (int j = 0; j < 100000; j++)
+    while (!isTrained)
     {
-        for (int i = 0; i < 4; i++)
-        {
-            // Feed the inputs forward through the network
-            forward_propagation(&network, *inputs);
+        forward_propagation(&network, *inputs);
 
-            // Get the outputs
-            Layer *output_layer = &network.layers[network.num_layers - 1];
-            double output = output_layer->neurons[i].output;
-
-            // Print the inputs, expected outputs, and obtained outputs
-            printf("Input: [%f, %f], Expected Output: %f, Obtained Output: %.2f\n", inputs[i][0], inputs[i][1], expected_outputs[i], output);
-        }
-
+        Layer *output_layer = &network.layers[network.num_layers - 1];
+        double output[] = {output_layer->neurons[0].output, output_layer->neurons[1].output, output_layer->neurons[2].output, output_layer->neurons[3].output};
         back_propagation(&network, expected_outputs);
+
+        isTrained = get_error_rate(&output, &expected_outputs);
     }
 
-    // Free the memory allocated for the network
-    for (int i = 0; i < network.num_layers; i++)
-    {
-        Layer *layer = &network.layers[i];
-        for (int j = 0; j < layer->num_neurons; j++)
-        {
-            Neuron *neuron = &layer->neurons[j];
-            free(neuron->weights);
-        }
-        free(layer->neurons);
-    }
-    free(network.layers);
+    Layer *output_layer = &network.layers[network.num_layers - 1];
+    double output[] = {output_layer->neurons[0].output, output_layer->neurons[1].output, output_layer->neurons[2].output, output_layer->neurons[3].output};
+
+    printf("\rOutput: %.2f, Expected: %f | Output: %.2f, Expected: %f | Output: %.2f, Expected: %f | Output: %.2f, Expected: %f", output[0], expected_outputs[0], output[1], expected_outputs[1], output[2], expected_outputs[2], output[3], expected_outputs[3]);
 
     return 0;
+}
+
+int get_error_rate(double *output, double *expected_outputs)
+{
+    int good_enough = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        double eoutput = expected_outputs[i];
+        double aoutput = output[i];
+        double err = eoutput - aoutput;
+        printf("\rError: %.4f", err);
+
+        if (fabs(err) > 0.0005)
+        {
+            good_enough = 0;
+        }
+    }
+    return good_enough;
 }
